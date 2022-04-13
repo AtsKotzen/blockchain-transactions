246bf75d30beb709b0e249cd39cbef7767adc22a
Athus <atsaudio@gmail.com>
2022-04-13 18:30:47 -0300

    Change Network to Polygon Mainnet

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Try the demo:
 ## To run on your machine you will need: 
 1. NodeJs and npm packages instaled
 2. INFURA account, if you don have one go to https://infura.io and create one.
-3. Change the INFURA_ETH_URL and WALLET_ADDRESS in .env.example file for your own.
+3. Change the INFURA_POLYGON_URL and WALLET_ADDRESS in .env.example file for your own.
 4. Rename file .env.example to .env
 5. Run npm install
 6. Run npm run start
--- api.js
+++ api.js
@@ -1,26 +1,26 @@
-const ethNetwork = process.env.INFURA_ETH_URL
+const polygonNetwork = process.env.INFURA_POLYGON_URL
 const Web3 = require('web3')
-const web3 = new Web3(new Web3.providers.HttpProvider(ethNetwork))
+const web3 = new Web3(new Web3.providers.HttpProvider(polygonNetwork))
 
-const getBlockNumber = function(){
-     web3.eth.getBlockNumber(function (error, result) {
-       try {
-           console.log('Block Number: ' + result)
-       } catch (error) {
-           console.log(error)
-       } 
-    });    
+const getBlockNumber = function () {
+  web3.eth.getBlockNumber(function (error, result) {
+    try {
+      console.log('Block Number: ' + result)
+    } catch (error) {
+      console.log(error)
+    }
+  });
 }
 
-const getBalanceFromAddress =  function(address){
-    web3.eth.getBalance(address, function(err, result) {
-        if (err) {
-          console.log(err)
-        } else {
-          let ethAmount = (web3.utils.fromWei(result, "ether"))
-          console.log("Address: " + address)
-          console.log(ethAmount + " ETH")  
-        }
-      })
+const getBalanceFromAddress = function (address) {
+  web3.eth.getBalance(address, function (err, result) {
+    if (err) {
+      console.log(err)
+    } else {
+      let ethAmount = (web3.utils.fromWei(result, "ether"))
+      console.log("Address: " + address)
+      console.log(ethAmount + " MATIC")
+    }
+  })
 }
 module.exports = { getBlockNumber, getBalanceFromAddress }
